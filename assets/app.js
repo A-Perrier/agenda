709d8f7c3950a8f11c92e7a833573cc31c00c9c2
Anthony Perrier <perrier_anthony@live.fr>
2021-06-11 17:22:51 +0200

    Configuration de la structure de base du SCSS. Création d'un service Twig pour charger les composants React avec une syntaxe qui se démarque davantage

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-
-// start the Stimulus application
-import './bootstrap';
-
-const Agenda = () => {
-  return <h1>Hello World !</h1>;
-}
-
-const rootElement = document.querySelector('#agenda');
-ReactDOM.render(<Agenda />, rootElement);
+// This line loads the main scss file of the app. In this one, there will be all of SCSS imports required
+// to structure the main app, excluding components themselves
+import './styles/app.scss'
---
+++ assets/components/Agenda.js
@@ -0,0 +1,80 @@
+import React, { createRef, useState } from 'react';
+import ReactDOM from 'react-dom';
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './Agenda.scss';
+
+// start the Stimulus application
+import '../bootstrap';
+
+
+const Agenda = () => {
+
+
+  return (
+  <>
+    <h1>Agenda</h1>
+
+    <div className="calendar">
+      <div className="month">
+        <div className="week">
+          <div className="day__prev-month">31</div>
+          <div className="day">1</div>
+          <div className="day">2</div>
+          <div className="day">3</div>
+          <div className="day">4</div>
+          <div className="day">5</div>
+          <div className="day">6</div>
+        </div>
+        <div className="week">
+          <div className="day">7</div>
+          <div className="day">8</div>
+          <div className="day">9</div>
+          <div className="day">10</div>
+          <div className="day">11</div>
+          <div className="day">12</div>
+          <div className="day">13</div>
+        </div>
+        <div className="week">
+          <div className="day">14</div>
+          <div className="day">15</div>
+          <div className="day">16</div>
+          <div className="day">17</div>
+          <div className="day">18</div>
+          <div className="day">19</div>
+          <div className="day">20</div>
+        </div>
+        <div className="week">
+          <div className="day">21</div>
+          <div className="day">22</div>
+          <div className="day">23</div>
+          <div className="day">24</div>
+          <div className="day">25</div>
+          <div className="day">26</div>
+          <div className="day">27</div>
+        </div>
+        <div className="week">
+          <div className="day">28</div>
+          <div className="day">29</div>
+          <div className="day">30</div>
+          <div className="day__next-month">1</div>
+          <div className="day__next-month">2</div>
+          <div className="day__next-month">3</div>
+          <div className="day__next-month">4</div>
+        </div>
+        <div className="week">
+          <div className="day__next-month">5</div>
+          <div className="day__next-month">6</div>
+          <div className="day__next-month">7</div>
+          <div className="day__next-month">8</div>
+          <div className="day__next-month">9</div>
+          <div className="day__next-month">10</div>
+          <div className="day__next-month">11</div>
+        </div>
+      </div>
+    </div>
+  </>);
+}
+
+const agenda = document.querySelector('#agenda');
+ReactDOM.render(<Agenda />, agenda);
